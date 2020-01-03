2bc2ead77151d8ca61f9055580e18b8cab7c090c
Jeremy Garnes <j06garnes@gmail.com>
2020-01-03 00:41:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,8 +66,6 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-:programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -84,7 +82,8 @@ def adding_matz
         }
      }
 
-    
+:programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+:programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"] 
 end
 
 def changing_alan
@@ -106,7 +105,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+     :alan_kay[:known_for] = :alans_new_info
+     :alan_kay[:alans_new_info] = "GUI"
 end
 
 def adding_to_dennis
